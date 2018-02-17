667ef97088587361ad814cc979887fe4c8ea4256
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-17 17:47:22 +0000

    installed stuff
    https://laravel-backpack.readme.io/docs/install-on-laravel-55

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -15,6 +15,12 @@
           <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
 
+           <!-- custom added menu items -->
+          <li><a href="{{ backpack_url('backup') }}"><i class="fa fa-hdd-o"></i> <span>Backups</span></a></li>
+          <li><a href="{{ backpack_url('log') }}"><i class="fa fa-terminal"></i> <span>Logs</span></a></li>
+
+          <li><a href="{{ backpack_url('setting') }}"><i class="fa fa-cog"></i> <span>Settings</span></a></li>
+          <li><a href="{{backpack_url('page') }}"><i class="fa fa-file-o"></i> <span>Pages</span></a></li>
           <!-- ======================================= -->
           {{-- <li class="header">Other menus</li> --}}
         </ul>
